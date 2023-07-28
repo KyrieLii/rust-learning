292e26ec4843f7a4a9873646851b16aff043bc73
limao2021 <limao2021@bytedance.com>
2023-07-28 09:16:33 +0800

    feat: varibables

---
+++ variables/target/debug/deps/variables-dbc21d710ad5fc40.d
@@ -0,0 +1,5 @@
+/Users/cain/Github/rust-learning/variables/target/debug/deps/variables-dbc21d710ad5fc40: src/main.rs
+
+/Users/cain/Github/rust-learning/variables/target/debug/deps/variables-dbc21d710ad5fc40.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/cain/Github/rust-learning/variables/target/debug/variables: /Users/cain/Github/rust-learning/variables/src/main.rs
